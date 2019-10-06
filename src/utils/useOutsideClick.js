0afcc4e799bfa7700a2066a7b1f03a460dff54cb
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-10-06 23:50:52 +0200

    Fix broken outside click When including a MUI picker in a modal, the ref set on the modal element would not include the picker, and thus the modal would close when clicking inside the picker. This fixes that.

--- src/utils/useOutsideClick.js
+++ src/utils/useOutsideClick.js
@@ -2,8 +2,13 @@ import { useEffect } from 'react'
 
 export default function useOutsideClick(ref, handleOutsideClick) {
   function performOutsideClick(event) {
+    const pickerNode = document.querySelector("div[role='presentation']");
     if (ref.current && !ref.current.contains(event.target)) {
-      handleOutsideClick()
+      if (!pickerNode) {
+        handleOutsideClick()
+      } else if (!pickerNode.contains(event.target)) {
+        handleOutsideClick() // To not treat MUI pickers as outside elements
+      }
     }
   }
 
